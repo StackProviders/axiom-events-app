f03736a21531a17f1bd21706a033bc61613c9b67
codewithrakibul <bdsky25@gmail.com>
2025-11-03 02:50:26 +0600

    feat(config): update websocket and target URLs for new pairs and price tracking
    - Renamed AXIOM_WS_URL to AXIOM_WS_NEW_PAIRS_URL and added AXIOM_WS_PRICE_TRACKER_URL for clarity.
    - Updated TARGET_URL to TARGET_NEW_PAIRS_URL and added TARGET_PRICE_TRACKER_URL for price tracking functionality.

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,8 +5,10 @@ export const config = {
     PORT: 5000,
     VISIBLE_BROWSER: true,
     PROFILE_PATH: path.join(process.cwd(), 'chrome-profile'),
-    AXIOM_WS_URL: ['wss://cluster9.axiom.trade', 'wss://cluster-global2.axiom.trade'],
-    TARGET_URL: 'https://axiom.trade/pulse?chain=sol',
+    AXIOM_WS_NEW_PAIRS_URL: ['wss://cluster9.axiom.trade', 'wss://cluster-global2.axiom.trade'],
+    AXIOM_WS_PRICE_TRACKER_URL: ['wss://cluster9.axiom.trade', 'wss://cluster-global2.axiom.trade'],
+    TARGET_NEW_PAIRS_URL: 'https://axiom.trade/pulse',
+    TARGET_PRICE_TRACKER_URL: 'https://axiom.trade/meme/',
     CHROMIUM_PATH: path.join(
         os.homedir(),
         'AppData',
